410411934128c77dbe2255f922b4ad42b0538bc7
StebanSzs <mbravod@unicauca.edu.co>
2024-08-23 20:54:57 -0500

    Animaciones en header y botones de sedes

--- js/js.js
+++ js/js.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelector('.nav.next').addEventListener('click', nextSlide);
     document.querySelector('.nav.prev').addEventListener('click', prevSlide);
 
-    setInterval(nextSlide, 5000); // Cambia cada 5 segundos
+    setInterval(nextSlide, 10000); 
 
     window.addEventListener('scroll', function() {
         var scrolled = window.pageYOffset;
@@ -27,4 +27,22 @@ document.addEventListener("DOMContentLoaded", function() {
         parallax.style.backgroundPositionY = -(scrolled * 0.5) + "px";
     });
     
+});
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 0) {
+        header.style.opacity = '0.8'; // Cambia la opacidad cuando se desplaza
+    } else {
+        header.style.opacity = '1'; // Opacidad completa en la parte superior
+    }
+});
+
+document.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) { // Cambia 50 por el valor que consideres adecuado
+        header.classList.add('shrink');
+    } else {
+        header.classList.remove('shrink');
+    }
 });
